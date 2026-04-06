987722b4bb7bb411cfc8c987882989cf0784b683
Nguyễn Huỳnh Thành Danh <thanhdanh130425@gmail.com>
2026-04-06 10:18:53 +0700

    feat: create folder utils

---
+++ calender/lib/utils/request.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String apiDomain = "http://192.168.80.1:3002/";
+
+  static Future<dynamic> get(String path) async {
+    final response = await http.get(Uri.parse(apiDomain + path));
+    return response;
+  }
+
+  static Future<dynamic> post(String path, Map<String, dynamic> body) async {
+    final response = await http.post(
+      Uri.parse(apiDomain + path),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode(body),
+    );
+    return response;
+  }
+
+  static Future<dynamic> del(String path) async {
+    final response = await http.delete(Uri.parse(apiDomain + path));
+    return response;
+  }
+
+  static Future<dynamic> patch(String path, Map<String, dynamic> body) async {
+    final response = await http.patch(
+      Uri.parse(apiDomain + path),
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode(body),
+    );
+    return response;
+  }
+}
